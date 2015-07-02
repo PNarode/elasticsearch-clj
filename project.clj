0ce3e9d48d9d811bad3694f2c011f064e59d5214
Pratik <pnarode@yieldbot.com>
2015-07-03 00:02:50 +0530

    Start of Project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.clojars.pnarode/elasticsearch-clj "0.1.0"
+  :description "A Clojure client for Elastic Search "
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+  				 [clj-http "0.9.1"]
+  				 [http-kit "2.1.18"]])
