5aa31a4d45d6f1fd7554a51fbd2e483f83e7d65d
Pratik Narode <pnarode@yieldbot.com>
2015-07-03 23:10:27 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.pnarode/elasticsearch-clj "0.1.0"
   :description "A Clojure client for Elastic Search "
-  :url "http:/"
+  :url "https://github.com/PNarode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
